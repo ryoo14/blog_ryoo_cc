4b370f592632248f3320f7b04310c7a930143646
ryoo14 <anana12185@gmail.com>
2020-10-04 21:08:22 +0900

    add if by article type

--- bin/new-post.sh
+++ bin/new-post.sh
@@ -1,10 +1,18 @@
 #!/bin/bash -u
 
-if [ "$#" -ne 1 ]; then
-  echo "Usage: new-post.sh <post-title>"
+function usage() {
+  echo "Usage: new-post.sh [post|draft] <title>"
   exit 1
+}
+
+if [ "$#" -ne 2 ]; then
+  usage
+elif [[ "$1" != "post" ]] && [[ "$1" != "draft" ]]; then
+  usage
 fi
 
+POST_TYPE=$1
+POST_TITLE=$2
 Y=$(date +"%Y")
 M=$(date +"%m")
 D=$(date +"%d")
@@ -12,7 +20,11 @@ SCRIPT_PATH=$(readlink -f $0)
 BIN_PATH=$(dirname $SCRIPT_PATH)
 BLOG_PATH=$(dirname $BIN_PATH)
 TEMP_FILE="${BIN_PATH}/yyyy-mm-dd-hoge-fuga.md"
-NEW_POST_FILE="${BLOG_PATH}/_posts/${Y}-${M}-${D}-${1}.md"
+if [[ "${POST_TYPE}" = "post" ]]; then
+  NEW_POST_FILE="${BLOG_PATH}/_posts/${Y}-${M}-${D}-${POST_TITLE}.md"
+else
+  NEW_POST_FILE="${BLOG_PATH}/_drafts/${Y}-${M}-${D}-${POST_TITLE}.md"
+fi
 POST_DATE=$(date +"%Y-%m-%d %H:%M:%S +0900")
 
 cp "$TEMP_FILE" "$NEW_POST_FILE"
